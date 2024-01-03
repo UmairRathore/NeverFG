0a7a4be9628b134c666d1cb43e4af3bb8997d533
Umair-bt <122082629+Umair-bt@users.noreply.github.com>
2024-01-03 05:18:18 +0500

    Roles

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminPortal\RoleController;
 use App\Http\Controllers\Authentication\AuthController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,9 +24,26 @@ Route::get('/',[\App\Http\Controllers\Authentication\RegistrationController::cla
 
 
 
+Route::get('/Dashboard',[DashboardController::class,'index'])->name('backend.index');
 Route::get('/signup',[AuthController::class]);
 Route::post('/signup',[AuthController::class]);
 Route::get('/login',[AuthController::class]);
 Route::post('/login',[AuthController::class]);
 Route::get('/forgot-password',[AuthController::class]);
 Route::post('/forgot-password',[AuthController::class]);
+
+
+
+//<----------CRUD ROLE
+
+Route::group(['prefix' => 'role'], function () {
+
+    Route::get('/role-list', [RoleController::class, 'index'])->name('backend.role-list');
+    Route::get('/add-role', [RoleController::class, 'create']);
+    Route::post('/add-role', [RoleController::class, 'store'])->name('backend.add-role');
+    Route::get('/edit-role/{id}', [RoleController::class, 'edit'])->name('backend.edit-role');
+    Route::patch('/update-role/{id}', [RoleController::class, 'update'])->name('backend.update-role');
+    Route::delete('/delete-role/{id}', [RoleController::class, 'destroy'])->name('backend.delete-role');
+
+});
+//Role
