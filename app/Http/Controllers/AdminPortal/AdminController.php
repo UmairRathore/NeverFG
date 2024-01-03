d394c04dcdd7f22c2453ec4b4617d7d5188e4a1a
Umair-bt <122082629+Umair-bt@users.noreply.github.com>
2024-01-03 05:29:41 +0500

    Roles

---
+++ app/Http/Controllers/AdminPortal/AdminController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\AdminPortal;
+
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+
+    //
+}
---
+++ app/Http/Controllers/AdminPortal/UserController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\AdminPortal;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    //
+}
