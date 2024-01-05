4e66589e7d46edc24d006960ae31fb332abdb244
Umair-bt <122082629+Umair-bt@users.noreply.github.com>
2024-01-06 04:09:25 +0500

    Web.php fixes

--- routes/web.php
+++ routes/web.php
@@ -37,31 +37,48 @@ Route::get('/memorialsignup',[RegistrationController::class,'memorialregistratio
 Route::post('/signup',[AuthController::class]);
 Route::get('/postLogin',[LoginController::class,'postLogin'])->name('postLogin');
 Route::post('/login',[AuthController::class]);
-Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('/forgot-password',[AuthController::class]);
 Route::post('/forgot-password',[AuthController::class]);
 
 
+Route::group(['prefix' => 'admin'], function () {
+
+
+//<----------CRUD User
+
+    Route::group(['prefix' => 'user'], function () {
+
+        Route::get('/list-user', [UserController::class, 'index'])->name('backend.list-user');
+        Route::get('/add-user', [UserController::class, 'create']);
+        Route::post('/add-user', [UserController::class, 'store'])->name('backend.add-user');
+        Route::get('/edit-user/{id}', [UserController::class, 'edit'])->name('backend.edit-user');
+        Route::patch('/update-user/{id}', [UserController::class, 'update'])->name('backend.update-user');
+        Route::get('/delete-user/{id}', [UserController::class, 'destroy'])->name('backend.delete-user');
+        Route::post('/status-user/{id}', [UserController::class, 'changeStatus'])->name('status-user');
+
+    });
+//User
 
 
 //<----------CRUD ROLE
 
-Route::group(['prefix' => 'role'], function () {
+    Route::group(['prefix' => 'role'], function () {
 
-    Route::get('/list-role', [RoleController::class, 'index'])->name('backend.role-list');
-    Route::get('/add-role', [RoleController::class, 'create']);
-    Route::post('/add-role', [RoleController::class, 'store'])->name('backend.add-role');
-    Route::get('/edit-role/{id}', [RoleController::class, 'edit'])->name('backend.edit-role');
-    Route::patch('/update-role/{id}', [RoleController::class, 'update'])->name('backend.update-role');
-    Route::get('/delete-role/{id}', [RoleController::class, 'destroy'])->name('backend.delete-role');
+        Route::get('/list-role', [RoleController::class, 'index'])->name('backend.role-list');
+        Route::get('/add-role', [RoleController::class, 'create']);
+        Route::post('/add-role', [RoleController::class, 'store'])->name('backend.add-role');
+        Route::get('/edit-role/{id}', [RoleController::class, 'edit'])->name('backend.edit-role');
+        Route::patch('/update-role/{id}', [RoleController::class, 'update'])->name('backend.update-role');
+        Route::get('/delete-role/{id}', [RoleController::class, 'destroy'])->name('backend.delete-role');
 
-});
+    });
 //Role
 
 
 //<----------CRUD Package
 
-//Route::group(['prefix' => 'package'], function () {
+Route::group(['prefix' => 'package'], function () {
 
     Route::get('/list-package', [PackageController::class, 'index'])->name('backend.package-list');
     Route::get('/add-package', [PackageController::class, 'create']);
@@ -69,21 +86,7 @@ Route::group(['prefix' => 'role'], function () {
     Route::get('/delete-package/{id}', [PackageController::class, 'destroy'])->name('backend.delete-package');
     Route::post('/status-package/{id}', [PackageController::class, 'changeStatus'])->name('backend.status-package');
 
-//});
+});
 //Package
 
-//<----------CRUD User
-
-Route::group(['prefix' => 'user'], function () {
-
-    Route::get('/list-user', [UserController::class, 'index'])->name('backend.list-user');
-    Route::get('/add-user', [UserController::class, 'create']);
-    Route::post('/add-user', [UserController::class, 'store'])->name('backend.add-user');
-    Route::get('/edit-user/{id}', [UserController::class, 'edit'])->name('backend.edit-user');
-    Route::patch('/update-user/{id}', [UserController::class, 'update'])->name('backend.update-user');
-    Route::get('/delete-user/{id}', [UserController::class, 'destroy'])->name('backend.delete-user');
-    Route::post('/status-user/{id}', [UserController::class, 'changeStatus'])->name('status-user');
-
 });
-//Role
-
