04838c08ad5690d2d8e2e1302f20db0ee0d4efa0
Umair-bt <122082629+Umair-bt@users.noreply.github.com>
2024-01-06 07:14:05 +0500

    Admin edit

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      */
 
     protected $table = 'users';
-    protected $fillable = [g
+    protected $fillable = [
         'first_name',
         'last_name',
         'email',
