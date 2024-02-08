3d5dc4fa095c4c912f4c372fb576725bceffa5a3
Umair-bt <122082629+Umair-bt@users.noreply.github.com>
2024-02-08 17:32:54 +0500

    privacyandterms crud not complete

---
+++ database/migrations/2024_02_08_120913_create_privacy_terms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrivacyTermsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('privacy_terms', function (Blueprint $table) {
+            $table->id();
+            $table->longText('privacy');
+            $table->longText('terms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('privacy_terms');
+    }
+}
