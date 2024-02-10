8e7f78790cbfd0695b4b8c7c361511d48a5e0ddc
Umair-bt <122082629+Umair-bt@users.noreply.github.com>
2024-02-11 01:36:48 +0500

    privacy and policy

--- resources/views/backend/frontend/index/create-frontend-index.blade.php
+++ resources/views/backend/frontend/index/create-frontend-index.blade.php
@@ -38,7 +38,7 @@
                                 @csrf
                                 <div class="row">
                                     <div class="col-md-6 mb-3">
-                                        <label for="index_image_heading">Title</label>
+                                        <label for="index_image_heading">Index Heading</label>
                                         <div class="input-group">
                                             <input type="text" name="index_image_heading" class="form-control @error('index_image_heading') is-invalid @enderror" value="{{ old('index_image_heading') }}" placeholder="Enter Title">
                                             @error('index_image_heading')
@@ -48,10 +48,9 @@
                                             @enderror
                                         </div>
                                     </div>
-                                </div>
-                                <div class="row">
+
                                     <div class="col-md-6 mb-3">
-                                        <label for="index_card_image_heading">Title</label>
+                                        <label for="index_card_image_heading">Index Card Heading</label>
                                         <div class="input-group">
                                             <input type="text" name="index_card_image_heading" class="form-control @error('index_card_image_heading') is-invalid @enderror" value="{{ old('index_card_image_heading') }}" placeholder="Enter Title">
                                             @error('index_card_image_heading')
@@ -65,6 +64,19 @@
 
 
                                 <div class="row">
+                                    <div class="col-md-12 mb-3">
+                                        <label for="index_card_image_description">Index Card Description</label>
+                                        <div class="input-group">
+                                            <textarea name="index_card_image_description" class="form-control @error('index_card_image_description') is-invalid @enderror" placeholder="Enter Description">{{ old('index_card_image_description') }}</textarea>
+                                            @error('index_card_image_description')
+                                            <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
                                     <div class="col-md-6 mb-3">
                                         <label for="index_image">Image</label>
                                         <div class="input-group">
@@ -72,9 +84,7 @@
                                             <span id="featureImageFileName"></span>
                                         </div>
                                     </div>
-                                </div>
 
-                                <div class="row">
                                     <div class="col-md-6 mb-3">
                                         <label for="index_card_image">Image</label>
                                         <div class="input-group">
@@ -83,8 +93,6 @@
                                         </div>
                                     </div>
                                 </div>
-
-
                                 <div>
                                     <input type="submit" class="btn btn-primary" value="Submit">
                                 </div>
