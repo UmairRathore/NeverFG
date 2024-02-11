0eeeda5373ef03dfa4f88115765b9cccd781d46c
Umair-bt <122082629+Umair-bt@users.noreply.github.com>
2024-02-11 18:19:25 +0500

    tweaks

---
+++ database/migrations/2024_02_10_155017_create_families_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFamiliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('families', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('relation');
+            $table->string('family_image')->nullable();
+            $table->unsignedBigInteger('memorial_user_id');
+            $table->timestamps();
+
+            $table->foreign('memorial_user_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('families');
+    }
+}
