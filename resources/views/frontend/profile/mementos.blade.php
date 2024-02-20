85b8582a5c5180581e277d545fa594d13b28d6a4
Umair-bt <122082629+Umair-bt@users.noreply.github.com>
2024-02-20 23:36:37 +0500

    images videos

--- resources/views/frontend/profile/mementos.blade.php
+++ resources/views/frontend/profile/mementos.blade.php
@@ -24,36 +24,36 @@
             <div class="form-of-logged-in-user">
                 <div class="header-of-form-profile margin-top">
                     <h1 class="form-top-main-heading-of-profile">Mementos</h1>
-{{--                    <p>With NeverFg, anyone that visits the page will only be able to view the 5 photos and videos you've selected--}}
-{{--                        below. When you get NeverFg Plus, all photos and videos will become visible to the public.</p>--}}
+                    {{--                    <p>With NeverFg, anyone that visits the page will only be able to view the 5 photos and videos you've selected--}}
+                    {{--                        below. When you get NeverFg Plus, all photos and videos will become visible to the public.</p>--}}
                 </div>
                 <div class="form-group-input">
-{{--                    --}}{{--                    <label for="">The profiles that {{ $keeper->first_name }} manages:</label>--}}
-{{--                    --}}{{--                    @foreach($memorials as $memorial)--}}
-{{--                                        <div id="profile-image-div" class="profile-pic-wrapper-of-Picture">--}}
-{{--                    @foreach ($mementos as $memento)--}}
-{{--                        @if ($loop->iteration % 3 == 1)--}}
-{{--                            <!-- Start a new row after every 3 images -->--}}
-{{--                            <div class="row">--}}
-{{--                                @endif--}}
-{{--                                <div class="col-md-4">--}}
-{{--                                    @if($memento->memento_image)--}}
-{{--                                        <img src="{{ asset($memento->memento_image) }}" alt="{{ $memento->memento_image }}" class="pic-of-usr">--}}
+                    {{--                    --}}{{--                    <label for="">The profiles that {{ $keeper->first_name }} manages:</label>--}}
+                    {{--                    --}}{{--                    @foreach($memorials as $memorial)--}}
+                    {{--                                        <div id="profile-image-div" class="profile-pic-wrapper-of-Picture">--}}
+                    {{--                    @foreach ($mementos as $memento)--}}
+                    {{--                        @if ($loop->iteration % 3 == 1)--}}
+                    {{--                            <!-- Start a new row after every 3 images -->--}}
+                    {{--                            <div class="row">--}}
+                    {{--                                @endif--}}
+                    {{--                                <div class="col-md-4">--}}
+                    {{--                                    @if($memento->memento_image)--}}
+                    {{--                                        <img src="{{ asset($memento->memento_image) }}" alt="{{ $memento->memento_image }}" class="pic-of-usr">--}}
 
-{{--                                    @else--}}
-{{--                                        <img src="{{asset('frontend/assets/images/hero_background_1.jpg')}}" alt="" class="pic-of-usr"/>--}}
+                    {{--                                    @else--}}
+                    {{--                                        <img src="{{asset('frontend/assets/images/hero_background_1.jpg')}}" alt="" class="pic-of-usr"/>--}}
 
-{{--                                    @endif--}}
-{{--                                </div>--}}
-{{--                                @if ($loop->iteration % 3 == 0 || $loop->last)--}}
-{{--                            </div>--}}
-{{--                        @endif--}}
-{{--                    @endforeach--}}
-{{--                                        </div>--}}
-{{--                    --}}{{--                        <a href="{{ route('profile', ['id' => $memorial->memorialID]) }}" class="profile-link">--}}
-{{--                    --}}{{--                            <span class="profile-name">{{ $memorial->first_name.' '.$memorial->last_name }}</span>--}}
-{{--                    --}}{{--                        </a>--}}
-{{--                    --}}{{--                    @endforeach--}}
+                    {{--                                    @endif--}}
+                    {{--                                </div>--}}
+                    {{--                                @if ($loop->iteration % 3 == 0 || $loop->last)--}}
+                    {{--                            </div>--}}
+                    {{--                        @endif--}}
+                    {{--                    @endforeach--}}
+                    {{--                                        </div>--}}
+                    {{--                    --}}{{--                        <a href="{{ route('profile', ['id' => $memorial->memorialID]) }}" class="profile-link">--}}
+                    {{--                    --}}{{--                            <span class="profile-name">{{ $memorial->first_name.' '.$memorial->last_name }}</span>--}}
+                    {{--                    --}}{{--                        </a>--}}
+                    {{--                    --}}{{--                    @endforeach--}}
 
                     <form id="imageUploadForm">
                         <div class="form-group-input">
@@ -64,6 +64,11 @@
                         <div class="footer-of-form-content">
                             <button type="button" id="uploadImageButton" class="form-btn">Upload Image</button>
                         </div>
+                        <div id="imageLoader" style="display: none;">
+                            <img src="{{asset('assets/loader.gif')}}" alt="Loader GIF">
+                            <p>Loading...</p>
+                        </div>
+                        <div id="imageMessage" style="display: none;"></div>
                     </form>
 
                     <form id="videoUploadForm">
@@ -75,32 +80,17 @@
                         <div class="footer-of-form-content">
                             <button type="button" id="uploadVideoButton" class="form-btn">Upload Video</button>
                         </div>
+                        <div id="videoLoader" style="display: none;">
+                            <img src="{{asset('assets/loader.gif')}}" alt="Loader GIF">
+                            <p>Loading...</p>
+                        </div>
+                        <div id="videoMessage" style="display: none;"></div>
                     </form>
-
 
 
 
                 </div>
             </div>
-
-{{--            <div class="form-of-logged-in-user linear-background-of-form">--}}
-{{--                <div class="header-of-form-profile margin-top">--}}
-{{--                    <h1 class="form-top-main-heading-of-profile">Better Mementos with NeverFg Plus</h1>--}}
-
-{{--                    @if(auth()->check())--}}
-{{--                        @if(auth()->user()->account_type_id == 1)--}}
-{{--                            <p>When you upgrade to NeverFg Plus, your friends and family will be able to view all uploaded Mementos and--}}
-{{--                                download their own copy of these pictures in a single file. With NeverFg Plus you change the order in which--}}
-{{--                                Memento images and videos appear. NeverFg Plus also enables you to upload full HD videos directly from your--}}
-{{--                                phone, tablet or computer.</p>--}}
-{{--                            <a href="https://buy.stripe.com/test_14k2a63F13IqfcYfYZ" class="black-background-btn" target="_blank">Upgrade NeverFg Plus</a>--}}
-{{--                        @elseif(auth()->user()->account_type_id == 2)--}}
-{{--                            <p>You already have the package</p>--}}
-{{--                            <button class="black-background-btn" onclick="showAlert('You already have the package.');">Upgrade NeverFg Plus</button>--}}
-{{--                        @endif--}}
-{{--                    @endif--}}
-{{--                </div>--}}
-{{--            </div>--}}
 
 
         </div>
@@ -118,15 +108,20 @@
 
         $(document).ready(function () {
             $('#uploadImageButton').click(function () {
-                uploadMemento('imageUploadForm');
+                uploadMemento('imageUploadForm', '#imageLoader', '#imageMessage');
             });
 
             $('#uploadVideoButton').click(function () {
-                uploadMemento('videoUploadForm');
+                uploadMemento('videoUploadForm', '#videoLoader', '#videoMessage');
             });
         });
 
-        function uploadMemento(formId) {
+        function uploadMemento(formId, loaderId, messageId) {
+            // Show loader
+            $(loaderId).show();
+            // Hide previous message
+            $(messageId).hide();
+
             // Get form data
             var formData = new FormData($('#' + formId)[0]);
             formData.append('_token', '{{ csrf_token() }}');
@@ -139,15 +134,28 @@
                 processData: false,
                 contentType: false,
                 success: function (response) {
+                    // Hide loader on success
+                    $(loaderId).hide();
+
+                    // Show success message
+                    $(messageId).text(response.message).addClass('success').show();
+
                     // Handle success
                     console.log(response);
                 },
                 error: function (xhr, status, error) {
+                    // Hide loader on error
+                    $(loaderId).hide();
+
+                    // Show error message
+                    $(messageId).text(xhr.responseJSON.message).addClass('error').show();
+
                     // Handle error
                     console.error(xhr.responseText);
                 }
             });
         }
+
 
 
 
